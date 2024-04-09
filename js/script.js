c0776656f6d56c0a47c4383128cb816e28fbfbb7
rng4302 <rajgariwala40302@gmail.com>
2024-04-09 13:44:03 +1000

    First commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "images/kitten-2.png",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
-    },
-    {
-        title: "Kitten 3",
-        image: "images/kitten-3.png",
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
-    }
-]
+const getcards = () => {
+    $.get('/api/cards', (response) => {
+        if (response.statusCode == 200) {
+            addCards(response.data);
+        }
+    })
+}
 
 const addCards = (items) => {
     items.forEach(item => {
@@ -33,19 +26,20 @@ const clickMe = () => {
 
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.path = $('#path').val();
+    formData.desciption = $('#description').val();
 
     console.log("Form Data Submitted: ", formData);
+    postCat(formData);
 }
 
 $(document).ready(function () {
     $('.materialboxed').materialbox();
-    addCards(cardList);
-    $('.modal').modal();
     $('#formSubmit').click(() => {
         submitForm();
     })
-})
+    $('.modal').modal();
+    //getcards();
+});
--- server.js
+++ server.js
@@ -1,11 +1,62 @@
 var express = require("express")
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
 var app = express()
-app.use(express.static(__dirname))
+const uri = "mongodb+srv://Raj:root@cluster0.aj2mz9k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+var port = process.env.port || 3000;
+let collection;
+
+app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function runDBConnection() {
+    try {
+        await client.connect();
+        collection = client.db().collection('Cat');
+        console.log(collection);
+    } catch (ex) {
+        console.error(ex);
+    }
+}
+
+const cardList = [
+    {
+        title: "Kitten 2",
+        image: "images/",
+        link: "About Kitten 2",
+        desciption: "Demo desciption about kitten 2"
+    },
+    {
+        title: "Kitten 3",
+        image: "images/kitten-3.png",
+        link: "About Kitten 3",
+        desciption: "Demo desciption about kitten 3"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+app.get('/api/cards', (req, res) => {
+    getAllCats((err, result) => {
+        if (!err) {
+            res.json({
+                statusCode: 200, data: result, message: 'get all cards success'
+            });
+        }
+    });
+});
 
 var port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log("App listening to:" + port)
+    runDBConnection();
 })
